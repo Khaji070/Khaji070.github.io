6ff36a87e8db8dc0ff5ed9944434701a30e8818b
Khaji070 <66924422+Khaji070@users.noreply.github.com>
2020-06-19 16:42:12 +0300

    Add files via upload

---
+++ sublime/js/main.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+
+	
+	$('.fancybox').fancybox({
+		openEffect : 'none',
+		closeEffect : 'none',
+		prevEffect : 'none',
+		nextEffect : 'none',
+
+		arrows : false,
+		helpers : {
+			media : {},
+			buttons : {}
+		}
+	});
+
+
+
+	
+	wow = new WOW(
+	    {
+		  animateClass: 'animated',
+		  mobile: false,
+		  offset: 100
+		}
+	);
+	wow.init();
+
+});
+
